c6aeaa5a81c07ce98eeb69a5f8413332081a2379
Roee Hashai <roee.hashai@gmail.com>
2024-02-24 21:44:42 +0200

    added friends to a new user creation

--- models/user.js
+++ models/user.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
+const { Schema } = mongoose;
+
 const userSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true},
-    image: {type: String, required: true}
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    image: { type: String, required: true },
+    friends: [{
+        friendId: { type: Schema.Types.ObjectId, ref: 'User' },
+        status: { type: String, enum: ['pending', 'approved'], default: 'pending' }
+    }]
 }
 );
 
