d8258877b9c490e9ae1e346ac4939d6996db25c0
Roee Hashai <roee.hashai@gmail.com>
2024-04-15 20:00:23 +0300

    added the correct init proccess

--- services/urlFilterServices.js
+++ services/urlFilterServices.js
@@ -1,93 +1,115 @@
 import http from 'http';
 
 function sendRequest(message) {
-    return new Promise((resolve, reject) => {
-      const postData = message;
-      const options = {
-        hostname: 'localhost',
-        port: process.env.BLOOM_FILTER_PORT,
-        path: '/',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'text/plain',
-          'Connection': 'keep-alive', // Keep the connection open for future requests
-          'Content-Length': Buffer.byteLength(postData)
-        }
-      };
-  
-      const req = http.request(options, (res) => {
-        let data = '';
-        res.on('data', (chunk) => {
-          data += chunk;
-        });
-        res.on('end', () => {
-          resolve(data);
-        });
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: 'localhost',
+      port: process.env.BLOOM_FILTER_PORT,
+      path: '/',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/plain',
+        'Connection': 'keep-alive', // Keep the connection open for future requests
+        'Content-Length': Buffer.byteLength(message)
+      }
+    };
+
+    const req = http.request(options, (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
       });
-  
-      req.on('error', (e) => {
-        console.error(`Problem with request: ${e.message}`);
-        reject(e);
+      res.on('end', () => {
+        resolve(data);
       });
-  
-      // Write data to request body
-      req.write(postData);
-      req.end();
     });
-  }
+
+    req.on('error', (e) => {
+      console.error(`Problem with request: ${e.message}`);
+      reject(e);
+    });
+
+    // Write data to request body
+    req.write(message);
+    req.end();
+  });
+}
 
 function findUrls(postContent) {
-    const urlRegex = /(\b((https?|ftp|file):\/\/|www\.)[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|](\b|$))/ig;
-    let urls = [];
-    let match;
+  const urlRegex = /(\b((https?|ftp|file):\/\/|www\.)[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|](\b|$))/ig;
+  let urls = [];
+  let match;
 
-    while ((match = urlRegex.exec(postContent)) !== null) {
-        urls.push(match[0]);
-    }
-    return urls;
+  while ((match = urlRegex.exec(postContent)) !== null) {
+    urls.push(match[0]);
+  }
+  return urls;
 }
 
 const validateUrl = async (url) => {
-    try {
-        const bloomFilterResponse = await sendRequest(url);
-        if (bloomFilterResponse.includes('blacklisted')) {
-            return false;
-        }
-        return true;
-    } catch (error) {
-        return false;
+  try {
+    const bloomFilterResponse = await sendRequest('2 ' + url);
+    if (bloomFilterResponse.includes('blacklisted')) {
+      return false;
     }
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
 const validateUrls = async (urls) => {
-    for (const url of urls) {
-        const isValid = await validateUrl(url);
-        if (!isValid) {
-            const endConnectionSucceffuly = await sendRequest('quit');
-            if (endConnectionSucceffuly.includes('Server connection closed')) {
-                return false;
-            }
-        }
+  for (const url of urls) {
+    const isValid = await validateUrl(url);
+    if (!isValid) {
+      const endConnectionSucceffuly = await sendRequest('quit');
+      if (endConnectionSucceffuly.includes('Server connection closed')) {
+        return false;
+      }
     }
-    const endConnectionSucceffuly = await sendRequest('quit');
-    if (endConnectionSucceffuly.includes('Server connection closed')) {
-        return true;
-    }
+  }
+  const endConnectionSucceffuly = await sendRequest('quit');
+  if (endConnectionSucceffuly.includes('Server connection closed')) {
+    return true;
+  }
 }
 
 const validateContent = async (content) => {
-    let postUrls = findUrls(content);
-    if (postUrls.length > 0) {
-        const isValid = await validateUrls(postUrls);
-        if (!isValid) {
-            const error = new Error('Post contains blacklisted URL');
-            error.code = 400;
-            throw error;
-        }
+  let postUrls = findUrls(content);
+  if (postUrls.length > 0) {
+    const isValid = await validateUrls(postUrls);
+    if (!isValid) {
+      const error = new Error('Post contains blacklisted URL');
+      error.code = 400;
+      throw error;
     }
-    return true;
+  }
+  return true;
+}
+
+const initBloomFilter = async () => {
+  try {
+    const bloomFilterResponse = await sendRequest(process.env.BF_INIT_CONFIG);
+    if (bloomFilterResponse.includes('Bloom filter initialized')) {
+      const urls = process.env.BLACKLISTED_URLS.split(',');
+      for (const url of urls) {
+        const res = await sendRequest('1 ' + url);
+        if (!res.includes('URL added')) {
+          return false;
+        }
+      }
+      const quitReq = await sendRequest('quit');
+      if (quitReq.includes('closing connection')) {
+        return true;
+      }
+    }
+  }
+  catch (error) {
+    return false;
+  }
 }
 
 export default {
-    validateContent
+  validateContent,
+  initBloomFilter
 };
--- set-config.js
+++ set-config.js
@@ -6,8 +6,10 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const configFolderPath = join(__dirname, 'config');
 const envContent = `CONNECTION_STRING='mongodb://localhost:27017/'
 PORT=8080
-JWT_SECRET='your_secret_key_here
-BLOOM_FILTER_PORT = 8081'
+JWT_SECRET='secret'
+BLOOM_FILTER_PORT = 8081
+BF_INIT_CONFIG = '128 1 2'
+BLACKLISTED_URLS = 'https://www.google.com,https://www.youtube.com,https://www.facebook.com,http://www.amazon.com,http://www.wikipedia.org,https://www.twitter.com,https://www.instagram.com,https://www.linkedin.com,https://www.netflix.com,http://www.apple.com,https://www.bbc.com,https://www.microsoft.com,http://www.imdb.com,https://www.cnn.com,https://www.espn.com,https://www.adobe.com,https://www.tumblr.com,http://www.pinterest.com,https://www.paypal.com,https://www.wordpress.com,https://www.ebay.com,http://www.reddit.com,https://www.dropbox.com,https://www.spotify.com,https://www.github.com,http://www.quora.com,https://www.yahoo.com,https://www.soundcloud.com,https://www.blogger.com,http://www.imgur.com'
 `;
 
 // Function to create a directory if it doesn't exist
